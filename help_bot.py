4dc2a2d38c6ceb7f55e949a9ab27f660f2f8c700
valera1adv <113802819+valera1adv@users.noreply.github.com>
2023-10-30 00:41:32 +0400

    Update help_bot.py

--- help_bot.py
+++ help_bot.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('6597827583:AAGCGNWxfEPQAcDUmZasaF0sufxOTzx-ZzI')
+bot = telebot.TeleBot('*********************************')
 
 @bot.message_handler(commands=['start'])
 def start(message):
